236a64c20277cbd11faad005753327cea5c48f9d
haidarah-amar <192092188+haidarah-amar@users.noreply.github.com.>
2026-03-11 10:53:01 +0300

    add success stories controller

--- app/Http/Controllers/SuccessStoryController.php
+++ app/Http/Controllers/SuccessStoryController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SuccessStory;
 use Illuminate\Http\Request;
 
 class SuccessStoryController extends Controller
 {
-    //
+    public function index() {
+        $successStories = SuccessStory::paginate(10);
+        return response()->json($successStories);
+    }
+    public function show($id) {
+        $successStory = SuccessStory::findOrFail($id);
+        return response()->json($successStory);
+    }
 }
