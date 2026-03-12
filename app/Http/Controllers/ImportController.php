3d708d75e4975c86437c65847c681eec4db4b206
haidarah-amar <192092188+haidarah-amar@users.noreply.github.com.>
2026-03-12 12:33:36 +0300

    edit the importing controller

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Imports\DonorsImport;
+use App\Imports\ProjectsImport;
 
 class ImportController extends Controller
 {
-    //
+
+    public function importDonors(Request $request)
+    {
+        $request->validate([
+            'file' => 'required|file|mimes:xlsx,xls,csv'
+        ]);
+
+        Excel::import(new DonorsImport, $request->file('file'));
+
+        return response()->json([
+            'message' => 'Donors imported successfully'
+        ]);
+    }
+
+
+    // public function importProjects(Request $request)
+    // {
+    //     $request->validate([
+    //         'file' => 'required|file|mimes:csv,txt'
+    //     ]);
+
+    //     Excel::import(new ProjectsImport, $request->file('file'));
+
+    //     return response()->json([
+    //         'message' => 'Projects imported successfully'
+    //     ]);
+    // }
+
 }
